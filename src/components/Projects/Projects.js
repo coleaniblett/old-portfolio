745c6412ebc6b1bcf9d8ed5035a2f295970fb856
ColeANiblett <coleaniblett@gmail.com>
2022-12-08 15:06:16 -0800

    add images to projects, reformat for cross-platform viewing

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Projects.css';
+import Katoche from '../../KatocheProjectImg.gif';
+import Portfolio from '../../PortfolioProjectImg.gif';
 
 class Projects extends React.Component {
-  render() {
+  /*render() {
     return (
       <div className="main-content">
         <h2 className="title">
@@ -20,6 +22,42 @@ class Projects extends React.Component {
               <li>Parses user input to determine player intent</li>
               <li>Utilizes OOP principles and design</li>
             </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }*/
+
+  render() {
+    return (
+      <div className="main-content">
+        <h2 className="title">
+          Projects
+        </h2>
+        <div className="project-grid">
+          <div className="project" id="katoche">
+            <div className="title">
+              <p>Katoche</p>
+            </div>
+            <img class="projectImg" src={Katoche}/>
+            <div className="description">
+              <ul>
+                <li>Text-based adventure game built with C++</li>
+                <li>Parses user input to determine player intent</li>
+                <li>Utilizes OOP principles and design</li>
+              </ul>
+            </div>
+          </div>
+          <div className="project" id="portfolio">
+            <div className="title">
+              <p>Portfolio</p>
+            </div>
+            <img class="projectImg" src={Portfolio}/>
+            <div className="description">
+              <ul>
+                <li>Front-end web app built with React</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>

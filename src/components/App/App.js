e68901bd11c4c490818630be33a2622da00d43a1
ColeANiblett <coleaniblett@gmail.com>
2022-12-14 17:23:08 -0800

    remove unnecessary variables

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,3 @@
-import logo from '../../logo.svg';
-import Navbar from '../Navbar/Navbar';
-import Home from '../Home/Home';
-import Education from '../Education/Education';
-import Projects from '../Projects/Projects';
-import Experience from '../Experience/Experience';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
 
@@ -11,11 +5,6 @@ function App() {
   return (
     <div className="App">
       <Main />
-      {/*<Navbar />
-      <Home />
-      <Education />
-      <Projects />
-      <Experience />*/}
       <Footer />
     </div>
   );

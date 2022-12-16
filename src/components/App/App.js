3800bebdace44a5450d58ec226ff60b22269131e
ColeANiblett <coleaniblett@gmail.com>
2022-12-15 20:24:44 -0800

    add check for mobile devices

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ const isMobile = /Mobi/.test(navigator.userAgent);
 function App() {
   return (
     <div className="App">
-      {console.log(`isMobile: ${isMobile}`)}
+      {window.alert(`isMobile: ${isMobile}`)}
       <Main />
       <Footer />
     </div>

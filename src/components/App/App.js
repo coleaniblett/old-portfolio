ecc584e7e4499170665737094fa49d8ccf63bed6
ColeANiblett <coleaniblett@gmail.com>
2022-12-15 19:43:07 -0800

    add check for mobile devices

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
 
+const isMobile = /Mobi/.test(navigator.userAgent);
+
 function App() {
   return (
     <div className="App">
+      {console.log(`isMobile: ${isMobile}`)}
       <Main />
       <Footer />
     </div>

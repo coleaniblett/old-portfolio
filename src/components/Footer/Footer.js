ebbbf4125540b2569921ead204260c24dfbde37c
ColeANiblett <coleaniblett@gmail.com>
2022-12-17 15:07:51 -0800

    fix typo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     return (
       <footer>
         <span>408-603-6257</span>
-        <span><a href="mailti:coleaniblett@gmail.com">coleaniblett@gmail.com</a></span>
+        <span><a href="mailto:coleaniblett@gmail.com">coleaniblett@gmail.com</a></span>
         <span><a href="https://github.com/coleaniblett">github.com/coleaniblett</a></span>
       </footer>
     );

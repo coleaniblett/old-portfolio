9305f36aa52f4cde250c30902cc2c90e20af5e4e
ColeANiblett <coleaniblett@gmail.com>
2022-12-30 10:27:41 -0800

    separate project data from display component

---
+++ src/components/ProjectData/ProjectData.js
@@ -0,0 +1,27 @@
+import Katoche from '../../KatocheProjectImg.gif';
+import Portfolio from '../../PortfolioProjectImg.gif';
+import DCRA from '../../DCRAProjectImg.gif';
+
+export const ProjectData = [
+  {
+    name: "The Distillation-Combination-Recommendation-Application",
+    src: DCRA,
+    bulletpoint1: "Front-end web app built with React",
+    bulletpoint2: "Utilizes TheCocktailDB API for user-customized drink recommendations",
+    bulletpoint3: null
+  },
+  {
+    name: "Katoche",
+    src: Katoche,
+    bulletpoint1: "Text-based adventure game built with C++",
+    bulletpoint2: "Parses user input to determine player intent",
+    bulletpoint3: "Utilizes OOP principles and design"
+  },
+  {
+    name: "Portfolio",
+    src: Portfolio,
+    bulletpoint1: "Front-end web app built with Bootstrap-React",
+    bulletpoint2: "Dynamically-designed for cross-platform viewing",
+    bulletpoint3: null
+  }
+];
